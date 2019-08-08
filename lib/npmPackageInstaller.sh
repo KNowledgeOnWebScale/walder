7f25b0fc1cd4381331840f3dbf4f703b0dbaae4e
Dries Marzougui <dries.marzougui@gmail.com>
2019-08-08 11:36:00 +0200

    added package.json generation script, closing #12

---
+++ lib/npmPackageInstaller.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Walter - NPM package installer
+#
+# Asks if a 'npm init' is required and runs it if needed.
+# Installs all NPM packages in the packages.txt file.
+
+
+input="./packages.txt"
+
+read -p "Is this project require a package.json file? [yes]" initRequirement
+initRequirement=${initRequirement:-yes}
+
+while [[ ${initRequirement} != "y" ]] && [[ ${initRequirement} != "n" ]] && [[ ${initRequirement} != "yes" ]] && [[ ${initRequirement} != "no" ]]
+do
+    read -p "Please answer with 'yes'/'y' (default) or 'no'/'n'" initRequirement
+done
+
+if [[ ${initRequirement} == "yes" || ${initRequirement} == "y" ]]; then
+    npm init
+fi
+
+while IFS= read -r line
+do
+    npm install --save ${line}
+done < "$input"
