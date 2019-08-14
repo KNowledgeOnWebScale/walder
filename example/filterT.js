c4fe469e7804853854b796ebc6857964dd781874
Dries Marzougui <dries.marzougui@gmail.com>
2019-08-14 10:08:07 +0200

    only local pipe modules - no copying

--- example/filterT.js
+++ example/filterT.js
@@ -1,10 +1,9 @@
-function filterT(data) {
+module.exports.filterT = (data) => {
   let filteredData = {data: []};
   for (const o of data.data) {
-    console.log(o);
     if (o.id.match(/T/)) {
       filteredData.data.push(o);
     }
   }
   return filteredData;
-}
+};
