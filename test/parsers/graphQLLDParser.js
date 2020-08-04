7525ec991aa645726542d65c8c34735850454f65
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-08-04 07:53:16 +0200

    fix #71

--- test/parsers/graphQLLDParser.js
+++ test/parsers/graphQLLDParser.js
@@ -12,10 +12,10 @@ describe('GraphQLLDParser', function () {
     const file = fs.readFileSync(path.resolve(__dirname, CONFIG_FILE), 'utf8');
     this.yamlData = YAML.parse(file);
 
-
     this.output = GraphQLLDParser.parse(this.yamlData.paths['/movies/{actor}']['get']['x-walder-query'], this.yamlData['x-walder-datasources'], true);
-    this.sorting_options_output = GraphQLLDParser.parse(this.yamlData.paths['/music/{musician}/sorted']['get']['x-walder-query'], this.yamlData['x-walder-datasources'], true);
-    this.duplicate_removal_options_output = GraphQLLDParser.parse(this.yamlData.paths['/music/{musician}/no_duplicates']['get']['x-walder-query'], this.yamlData['x-walder-datasources'], true);
+    this.sortingOptionsOutput = GraphQLLDParser.parse(this.yamlData.paths['/music/{musician}/sorted']['get']['x-walder-query'], this.yamlData['x-walder-datasources'], true);
+    this.duplicateRemovalOptionsOutput = GraphQLLDParser.parse(this.yamlData.paths['/music/{musician}/no_duplicates']['get']['x-walder-query'], this.yamlData['x-walder-datasources'], true);
+    this.smallerContextOutput = GraphQLLDParser.parse(this.yamlData.paths['/movies2/{actor}']['get']['x-walder-query'], this.yamlData['x-walder-datasources'], true);
   });
 
   describe('#functionality()', function () {
@@ -39,90 +39,115 @@ describe('GraphQLLDParser', function () {
               "starring": "http://dbpedia.org/ontology/starring"
             }
           },
-          "queries": { 'data': {'query': "{ id @single ... on Film { starring(label: $actor) @single }}"} },
+          "queries": {'data': {'query': "{ id @single ... on Film { starring(label: $actor) @single }}"}},
         }
       )
     });
 
     it('should be able to parse sorting options correctly from a YAML config file', function () {
-      this.sorting_options_output.should.eql(
-          {
-            "parameters": [],
-            "cache": true,
-            "comunicaConfig": {
-              "sources": [
-                  "http://fragments.dbpedia.org/2016-04/en"
-              ]
-            },
-            "context": {
-              "@context": {
-                "label": "http://www.w3.org/2000/01/rdf-schema#label",
-                "label_en": {
-                  "@id": "http://www.w3.org/2000/01/rdf-schema#label",
-                  "@language": "en"
-                },
-                "writer": "http://dbpedia.org/ontology/writer",
-                "artist": "http://dbpedia.org/ontology/musicalArtist"
-              }
-            },
-            "queries": {
-              "data": {
-                  "query": "{ label @single writer(label_en: $musician) @single artist @single(scope: all) { label }}",
-                  "options": {
-                      "sort": {
-                          "object": "$[*]",
-                          "selectors": [
-                              {
-                                  "value": "label",
-                                  "order": "desc"
-                              }
-                          ]
-                      }
-                  }
+      this.sortingOptionsOutput.should.eql(
+        {
+          "parameters": [],
+          "cache": true,
+          "comunicaConfig": {
+            "sources": [
+              "http://fragments.dbpedia.org/2016-04/en"
+            ]
+          },
+          "context": {
+            "@context": {
+              "label": "http://www.w3.org/2000/01/rdf-schema#label",
+              "label_en": {
+                "@id": "http://www.w3.org/2000/01/rdf-schema#label",
+                "@language": "en"
+              },
+              "writer": "http://dbpedia.org/ontology/writer",
+              "artist": "http://dbpedia.org/ontology/musicalArtist"
+            }
+          },
+          "queries": {
+            "data": {
+              "query": "{ label @single writer(label_en: $musician) @single artist @single(scope: all) { label }}",
+              "options": {
+                "sort": {
+                  "object": "$[*]",
+                  "selectors": [
+                    {
+                      "value": "label",
+                      "order": "desc"
+                    }
+                  ]
+                }
               }
             }
           }
+        }
       )
     });
 
     it('should be able to parse duplicate removal options correctly from a YAML config file', function () {
-          this.duplicate_removal_options_output.should.eql(
-              {
-                "parameters": [],
-                  "cache": true,
-                  "comunicaConfig": {
-                      "sources": [
-                          "http://fragments.dbpedia.org/2016-04/en"
-                      ]
-                  },
-                  "context": {
-                      "@context": {
-                          "label": "http://www.w3.org/2000/01/rdf-schema#label",
-                          "label_en": {
-                              "@id": "http://www.w3.org/2000/01/rdf-schema#label",
-                              "@language": "en"
-                          },
-                          "writer": "http://dbpedia.org/ontology/writer",
-                          "artist": "http://dbpedia.org/ontology/musicalArtist"
-                      }
-                  },
-                  "queries": {
-                      "data": {
-                          "query": "{ label @single writer(label_en: $musician) @single artist @single(scope: all) { label }}",
-                          "options": {
-                              "remove-duplicates": {
-                                  "object": "$[*]",
-                                  "value": "label"
-                              }
-                          }
-                      }
-                  }
+      this.duplicateRemovalOptionsOutput.should.eql(
+        {
+          "parameters": [],
+          "cache": true,
+          "comunicaConfig": {
+            "sources": [
+              "http://fragments.dbpedia.org/2016-04/en"
+            ]
+          },
+          "context": {
+            "@context": {
+              "label": "http://www.w3.org/2000/01/rdf-schema#label",
+              "label_en": {
+                "@id": "http://www.w3.org/2000/01/rdf-schema#label",
+                "@language": "en"
+              },
+              "writer": "http://dbpedia.org/ontology/writer",
+              "artist": "http://dbpedia.org/ontology/musicalArtist"
+            }
+          },
+          "queries": {
+            "data": {
+              "query": "{ label @single writer(label_en: $musician) @single artist @single(scope: all) { label }}",
+              "options": {
+                "remove-duplicates": {
+                  "object": "$[*]",
+                  "value": "label"
+                }
               }
-          )
-      });
+            }
+          }
+        }
+      )
+    });
+
+    it('should be able to convert a smaller version of a context to normal version', function () {
+      this.smallerContextOutput.should.eql(
+        {
+          "parameters": [],
+          "cache": true,
+          "comunicaConfig": {
+            "sources": [
+              "http://fragments.dbpedia.org/2016-04/en"
+            ]
+          },
+          "context": {
+            "@context": {
+              "Film": "http://dbpedia.org/ontology/Film",
+              "label": {
+                "@id": "http://www.w3.org/2000/01/rdf-schema#label",
+                "@language": "en"
+              },
+              "starring": "http://dbpedia.org/ontology/starring"
+            }
+          },
+          "queries": { 'data': {'query': "{ id @single ... on Film { starring(label: $actor) @single }}"} },
+        }
+      )
+    });
   });
 
-    describe('#outputFormat()', function () {
+  describe('#outputFormat()', function () {
     it('output object should have { cache, comunicaConfig, context, query } properties', function () {
       this.output.should.have.property('cache');
       this.output.should.have.property('comunicaConfig');
