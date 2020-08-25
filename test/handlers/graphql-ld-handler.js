6f783166f352a1d83200c6e6a692011c308d281a
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-08-25 10:12:48 +0200

    fix #20

---
+++ test/handlers/graphql-ld-handler.js
@@ -0,0 +1,45 @@
+require('chai').should();
+const GraphQLLDHandler = require('../../lib/handlers/graphql-ld-handler');
+
+describe('GraphQLLDHandler', function () {
+
+  it('integer variable in query', () => {
+    const handler = new GraphQLLDHandler();
+    const query = '{ id @single name @single review @single { rating(value: $value) }}';
+    const variables = {
+      "value": "1"
+    };
+    const definedParameters = {
+      "value": {
+        "required": true,
+        "type": "integer",
+        "description": "Rating of the tv shows.",
+        "in": "path"
+      }
+    };
+    const expectedNewQuery = '{ id @single name @single review @single { rating(value: 1) }}';
+    const actualNewQuery = handler._substituteVariables(query, variables, definedParameters);
+
+    actualNewQuery.should.eql(expectedNewQuery);
+  });
+
+  it('string variable in query', () => {
+    const handler = new GraphQLLDHandler();
+    const query = '{ id @single name @single review @single { rating(value: $value) }}';
+    const variables = {
+      "value": "1"
+    };
+    const definedParameters = {
+      "value": {
+        "required": true,
+        "type": "string",
+        "description": "Rating of the tv shows.",
+        "in": "path"
+      }
+    };
+    const expectedNewQuery = '{ id @single name @single review @single { rating(value: "1") }}';
+    const actualNewQuery = handler._substituteVariables(query, variables, definedParameters);
+
+    actualNewQuery.should.eql(expectedNewQuery);
+  });
+});
